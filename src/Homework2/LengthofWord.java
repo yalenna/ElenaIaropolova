f5dc4ff45e9dcdba9d90b4321978a807d9c47cff
yalenna <lenayaropolova@gmail.com>
2023-03-15 18:28:13 +0300

    [002] - Homework 2 - LengthofWord

---
+++ src/Homework2/LengthofWord.java
@@ -0,0 +1,19 @@
+package Homework2;
+/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.*/
+public class LengthofWord {
+    public static int lengthOfLastWord(String s) {
+        {
+            s = s.trim();
+            int result = s.lastIndexOf(" ");
+            if (result == -1) {
+                return s.length();
+            }
+            return s.length() - result-1;
+        }
+    }
+    public static void main( String[] args ) {
+        String a = "  Hello my dear friend    ";
+        System.out.println(lengthOfLastWord(a));
+    }
+}
